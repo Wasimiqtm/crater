0aac5bc9e913c6f1b7d6dd507b8292537cd1c7b3
raza0268 <syedrazamehdi0@gmail.com>
2020-12-15 16:27:10 +0500

    customer module started 15-12-2020

--- resources/assets/js/views/customers/Create.vue
+++ resources/assets/js/views/customers/Create.vue
@@ -27,7 +27,96 @@
           </base-button>
         </div>
       </div>
+      <!-- /////// -->
       <div class="customer-card card">
+        <div class="card-body">
+          <div class="row">
+            <div class="text-center section-title col-sm-12">{{ $t('customers.customer_type') }}</div>
+            <div class="col-sm-12">
+              <div class="form-group">
+                <div>
+                  <button @click="showForm" class="btn btn-primary btn-outline" type="button" aria-pressed="false" id="private_btn">Private customer</button>
+                  <button @click="hideForm" class="btn btn-primary btn-outline" type="button" aria-pressed="false" id="business_btn">Business customer</button>
+                </div>
+                <!-- <label class="form-label">{{ $t('customers.display_name') }}</label><span class="text-danger"> *</span> -->
+                <!-- <base-input
+                  :invalid="$v.formData.name.$error"
+                  v-model="formData.name"
+                  focus
+                  type="text"
+                  name="name"
+                  tab-index="1"
+                  @input="$v.formData.name.$touch()"
+                />
+                <div v-if="$v.formData.name.$error">
+                  <span v-if="!$v.formData.name.required" class="text-danger">
+                    {{ $tc('validation.required') }}
+                  </span>
+                  <span v-if="!$v.formData.name.minLength" class="text-danger">
+                    {{ $tc('validation.name_min_length', $v.formData.name.$params.minLength.min, { count: $v.formData.name.$params.minLength.min }) }}
+                  </span>
+                </div> -->
+              </div>
+            </div>
+          </div>
+          <hr> <!-- second row complete  -->
+        </div>
+
+        <div v-if="isBusiness" class="card-body">
+          <div class="row">
+            <!-- <div class="text-center section-title col-sm-12">{{ $t('customers.customer_type') }}</div> -->
+            <div class="col-sm-12">
+              <div class="form-group">
+                <div>
+                  <ul>
+                    <li>
+                      <input v-model="removelines" :value="1" type="radio" id="f-option" name="selector">
+                      <label for="f-option">Company With Personal Liabaility</label>
+                      
+                      <div class="check"></div>
+                    </li>
+                    
+                    <li>
+                      <input v-model="removelines"  :value="2" type="radio" id="s-option" name="selector">
+                      <label for="s-option">Capital Company</label>
+                      
+                      <div class="check"></div>
+                    </li>
+                    
+                    <li>
+                      <input v-model="removelines"  :value="3" type="radio" id="t-option" name="selector">
+                      <label for="t-option">Public Company / Authority</label>
+                      
+                      <div class="check"></div>
+                    </li>
+                  </ul>
+                </div>
+                <!-- <label class="form-label">{{ $t('customers.display_name') }}</label><span class="text-danger"> *</span> -->
+                <!-- <base-input
+                  :invalid="$v.formData.name.$error"
+                  v-model="formData.name"
+                  focus
+                  type="text"
+                  name="name"
+                  tab-index="1"
+                  @input="$v.formData.name.$touch()"
+                />
+                <div v-if="$v.formData.name.$error">
+                  <span v-if="!$v.formData.name.required" class="text-danger">
+                    {{ $tc('validation.required') }}
+                  </span>
+                  <span v-if="!$v.formData.name.minLength" class="text-danger">
+                    {{ $tc('validation.name_min_length', $v.formData.name.$params.minLength.min, { count: $v.formData.name.$params.minLength.min }) }}
+                  </span>
+                </div> -->
+              </div>
+            </div>
+          </div>
+          <hr> <!-- second row complete  -->
+        </div>
+      </div>
+      <!-- /////////////// -->
+      <div v-if="isPrivate" class="customer-card card">
         <div class="card-body">
           <div class="row">
             <div class="section-title col-sm-2">{{ $t('customers.basic_info') }}</div>
@@ -351,6 +440,9 @@ export default {
   mixins: [validationMixin],
   data () {
     return {
+      isPrivate:false,
+      isBusiness:false,
+      removelines: 'b',
       isCopyFromBilling: false,
       isLoading: false,
       formData: {
@@ -493,6 +585,14 @@ export default {
     }
   },
   methods: {
+    showForm(){
+      this.isPrivate = true;
+      this.isBusiness = false;
+    },
+    hideForm(){
+      this.isBusiness = true;
+      this.isPrivate = true;
+    },
     currencyNameWithCode ({name, code}) {
       return `${code} - ${name}`
     },
@@ -619,3 +719,155 @@ export default {
   }
 }
 </script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Lato');
+
+body, html{
+  height: 100%;
+  background: #222222;
+	font-family: 'Lato', sans-serif;
+}
+
+.container{
+  display: block;
+  position: relative;
+  margin: 40px auto;
+  height: auto;
+  width: 500px;
+  padding: 20px;
+}
+
+h2 {
+	color: #0f0e0e;
+}
+
+.container ul{
+  list-style: none;
+  margin: 0;
+  padding: 0;
+	overflow: auto;
+}
+
+ul li{
+  color: #AAAAAA;
+  display: block;
+  position: relative;
+  float: left;
+  width: 100%;
+  height: 100px;
+	border-bottom: 1px solid #333;
+}
+
+ul li input[type=radio]{
+  position: absolute;
+  visibility: hidden;
+}
+
+ul li label{
+  display: block;
+  position: relative;
+  font-weight: 300;
+  font-size: 1.35em;
+  padding: 25px 25px 25px 80px;
+  margin: 10px auto;
+  height: 30px;
+  z-index: 9;
+  cursor: pointer;
+  -webkit-transition: all 0.25s linear;
+}
+
+ul li:hover label{
+	color: #4f55a1;
+}
+
+ul li .check{
+  display: block;
+  position: absolute;
+  border: 5px solid #AAAAAA;
+  border-radius: 100%;
+  height: 25px;
+  width: 25px;
+  top: 30px;
+  left: 20px;
+	z-index: 5;
+	transition: border .25s linear;
+	-webkit-transition: border .25s linear;
+}
+
+ul li:hover .check {
+  border: 5px solid #1e0153;
+}
+
+ul li .check::before {
+  display: block;
+  position: absolute;
+	content: '';
+  border-radius: 100%;
+  height: 15px;
+  width: 15px;
+  top: 5px;
+	left: 5px;
+  margin: auto;
+	transition: background 0.25s linear;
+	-webkit-transition: background 0.25s linear;
+}
+
+input[type=radio]:checked ~ .check {
+  border: 5px solid #1e0153;
+}
+
+input[type=radio]:checked ~ label{
+  color: #1e0153;
+}
+
+.signature {
+	margin: 10px auto;
+	padding: 10px 0;
+	width: 100%;
+}
+
+.signature p{
+	text-align: center;
+	font-family: Helvetica, Arial, Sans-Serif;
+	font-size: 0.85em;
+	color: #AAAAAA;
+}
+
+.signature .much-heart{
+	display: inline-block;
+	position: relative;
+	margin: 0 4px;
+	height: 10px;
+	width: 10px;
+	background: #AC1D3F;
+	border-radius: 4px;
+	-ms-transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+    transform: rotate(45deg);
+}
+
+.signature .much-heart::before, 
+.signature .much-heart::after {
+	  display: block;
+  content: '';
+  position: absolute;
+  margin: auto;
+  height: 10px;
+  width: 10px;
+  border-radius: 5px;
+  background: #AC1D3F;
+  top: -4px;
+}
+
+.signature .much-heart::after {
+	bottom: 0;
+	top: auto;
+	left: -4px;
+}
+
+.signature a {
+	color: #AAAAAA;
+	text-decoration: none;
+	font-weight: bold;
+}
+</style>
